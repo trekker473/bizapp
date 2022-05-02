a5f266bc0d88ae84a526e6f978c8313c08227728
trekker473 <trek_porter@yahoo.com>
2022-05-02 13:19:19 -0700

    fixed bootstrap by reverting to old

--- crm/models.py
+++ crm/models.py
@@ -1,25 +1,54 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from users.models import Profile
 # Create your models here.
 
-class Company(models.Model):
-    companyName =
+class Entity(models.Model):
+    entityName = models.CharField()
+    lastUpdated = models.DateTimeField()
+    description = models.TextField()
+    createdDateTime = models.DateTimeField()
 
     def __str__(self):
-        return companyName
+        return entityName
 
 class Person(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company = models.
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    person = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    entity = models.ForeignKey('Entity', on_delete=models.CASCADE)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    lastUpdated = models.DateTimeField()
 
     def __str__(self):
-        return f'(self.user.username)'
+        return f'(self.person.user.username)'
 
 class PhoneNumber(models.Model):
+    countryCode = models.IntergerField()
+    areaCode = models.IntergerField()
+    phoneNumber = models.IntergerField()
+    ext = models.IntergerField()
+    type = models.CharField()
+    personOwner = models.ManyToManyField()
+    entityOwner = models.ManyToManyField()
+
 
 class Address(models.Model):
+    address1 = models.CharField()
+    address2 = models.CharField()
+    stateOrProvince = models.CharField()
+    zipcode = models.CharField()
+    country = models.CharField()
+
 
 class Email(models.Model):
+    personOwner = models.ManyToManyField()
+    entityOwner = models.ManyToManyField()
+    type = models.CharField()
+    emailAddress = models.EmailField()
+
 
 class Note(models.Model):
+    createdDateTime = models.DateTimeField()
+    lastUpdated = models.DateTimeField()
+    content = models.TextField()
+    createdBy = models.ForeignKey()
